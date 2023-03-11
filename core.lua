714701c1ea0cbd44ef187777eb53c2b8cd587816
brandongevat <brandongevat@gmail.com>
2023-03-11 15:47:44 -0500

    .

--- core.lua
+++ core.lua
@@ -3,7 +3,6 @@ local frame = CreateFrame("Frame", "HealthstoneFrame", UIParent);
 -- Don't Care Anymore
 frame:RegisterEvent("BAG_NEW_ITEMS_UPDATED");
 frame:SetScript("OnEvent", function(self, event, ...)
-    print(event);
     if event == "BAG_NEW_ITEMS_UPDATED" then
         C_Timer.After(.5, function()
             local itemID = C_Container.GetContainerItemID;
@@ -12,7 +11,7 @@ frame:SetScript("OnEvent", function(self, event, ...)
                 for slot = 1, C_Container.GetContainerNumSlots(bag) do
                     if itemID(bag, slot) == 36892 or itemID(bag, slot) == 36893 or itemID(bag, slot) == 36894 then
                         id = itemID(bag, slot);
-                        local macroIndex = GetMacroIndexByName("HealthstoneHelper");
+                        local macroIndex = GetMacroIndexByName("Healthstone");
                         SetMacroItem(macroIndex, "item:"..id)
                     end
                 end
